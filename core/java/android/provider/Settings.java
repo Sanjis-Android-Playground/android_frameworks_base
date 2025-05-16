362eba5cabea636a1a86abea6438fdff4194291e
Sanjivns <113604406+Sanjivns@users.noreply.github.com>
2025-05-16 06:24:20 +0000

    nuke duplicate

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6580,13 +6580,10 @@ public final class Settings {
         /**
          * @hide
          */
-        public static final String ONGOING_ACTION_CHIP = "ongoing_action_chip";
-
         /**
          * @hide
          */
         public static final String SHOW_MEDIA_PROGRESS = "show_media_progress";
-        
         /**
          * @hide
          */
